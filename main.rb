d693d2231588cc823c46cdb7436aa7ec3498efb1
Peter Sutton <foxxy@foxdogstudios.com>
2014-02-06 21:21:57 +0000

    Use new node builder

--- main.rb
+++ main.rb
@@ -1,121 +1,79 @@
-require 'logger'
 require 'json'
 
 require 'citysdk'
 require 'trollop'
 
+
+def main
+  opts = parse_options()
+  url = opts.fetch(:url)
+  email = opts.fetch(:email)
+  password = opts.fetch(:password)
+
+  api = CitySDK::API.new(url)
+  api.set_credentials(email, password)
+
+  layer = opts.fetch(:layer)
+  unless api.layer?(layer)
+    api.create_layer(
+      name:         layer,
+      description:  opts.fetch(:description),
+      organization: opts.fetch(:organization),
+      category:     opts.fetch(:category)
+    )
+  end # unless
+
+  builder = CitySDK::NodeBuilder.new
+  builder.load_data_set_from_json!(opts.fetch(:input))
+  builder.set_geometry_from_lat_lon!('lat', 'lon')
+  builder.set_node_id_from_data_field!('id')
+  builder.set_node_name_from_data_field!('name')
+  nodes = builder.build
+
+  api.create_nodes(layer, nodes)
+  return 0
+end
+
+
 def parse_options
   opts = Trollop::options do
-    opt(:config,
-        'Configuration JSON file',
-        :type => :string)
-    opt(:username,
-        'City SDK username',
-        :type => :string)
-    opt(:password,
-        'City SDK password',
-        :type => :string)
-    opt(:host,
-        'City SDK endpoint hostname',
-        :type => :string)
-    opt(:layer,
-        'Layer name',
-        :type => :string)
-    opt(:description,
-        'Layer description',
-        :type => :string)
-    opt(:organization,
-        'Layer organization',
-        :type => :string)
-    opt(:category,
-        'Layer category',
-        :type => :string)
-    opt(:input,
-        'Input file to import',
-        :type => :string,
-        :required => true)
+    opt(:category    , 'Layer category'      , type: :string)
+    opt(:config      , 'Configuration file'  , type: :string)
+    opt(:description , 'Layer description'   , type: :string)
+    opt(:input       , 'Input file to import', type: :string, required: true)
+    opt(:layer       , 'Layer name'          , type: :string)
+    opt(:organization, 'Layer organization'  , type: :string)
+    opt(:password    , 'CitySDK password'    , type: :string)
+    opt(:url         , 'CitySDK API base URL', type: :string)
+    opt(:email       , 'CitySDK email'       , type: :string)
   end
 
-  if opts[:config]
-    config_file = opts[:config]
-    config = JSON.parse(IO.read(config_file), {symbolize_names: true})
+  if opts.key?(:config)
+    config = open(opts.fetch(:config)) do |config_file|
+      JSON.load(config_file, nil, symbolize_names: true)
+    end
     opts = opts.merge(config)
   end
 
-  [:username, :password, :host, :layer, :description, :organization,
-   :category].each do |option|
-    unless opts[option]
-      Trollop::die option, 'must be specified.'
-    end
+  required = [
+    :category,
+    :description,
+    :layer,
+    :organization,
+    :password,
+    :url,
+    :email
+  ]
+
+  required.each do |opt|
+    Trollop::die(opt, 'must be specified.') if opts[opt].nil?
   end
 
   opts
 end
 
-class CitySdkFileImporter
-  def initialize(host, username, password, layer_name, layer_description,
-                 layer_organization, layer_category)
-    @host = host
-    @username = username
-    @password = password
-    @layer_name = layer_name
-    @layer_description = layer_description
-    @layer_organization = layer_organization
-    @layer_category = layer_category
-  end
-
-  def ensure_layer_exists()
-    api = CitySDK::API.new(@host)
-    unless api.authenticate(@username, @password)
-      raise "Unable to authenticate user: #{@username} password #{@password}"
-    end
-    begin
-      result = api.get("/layer/#{@layer_name}")
-    rescue CitySDK::HostException
-      api.put('/layers', {
-        :data => {
-          :name => @layer_name,
-          :description => @layer_description,
-          :organization => @layer_organization,
-          :category => @layer_category
-        }
-      })
-    ensure
-      api.release
-    end
-  end
-
-  def import_file(file_path)
-    self.ensure_layer_exists()
-    importer = CitySDK::Importer.new({
-      :file_path => file_path,
-      :host => @host,
-      :email => @username,
-      :passw => @password,
-      :layername => @layer_name
-    })
-    importer.doImport
-  end
-
-end
-
-
-def main
-  opts = parse_options()
-  file_importer = CitySdkFileImporter.new(
-    opts[:host],
-    opts[:username],
-    opts[:password],
-    opts[:layer],
-    opts[:description],
-    opts[:organization],
-    opts[:category]
-  )
-  file_importer.import_file(opts[:input])
-end
-
 
 if __FILE__ == $0
-  main()
+  exit main
 end
 
